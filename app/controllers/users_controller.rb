b2c7148242fb48274c3532aa322c746736103075
Eric Klein <eric.j.klein@chicagobooth.edu>
2015-12-03 19:52:33 -0600

    current user can only edit and update himself

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,24 +19,33 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find( params[ :id] )
+    if current_user!=@user then
+      redirect_to '/users', :notice => "Can only edit yourself."
+    else
+      render 'edit'
+    end
   end
 
   def update
     @user = User.find(params[:id])
-
-    @user.gmat = params[:gmat]
-    @user.gpa = params[:gpa]
-    @user.industry = params[:industry]
-    @user.collegerank = params[:collegerank]
-    @user.timeinservice = params[:timeinservice]
-    @user.major = params[ :major ]
-    @user.prohelp = params[ :prohelp ]
-
-    if @user.save
-      redirect_to "/users", :notice => "My stats were updated successfully."
+    if current_user!=@user then
+      redirect_to '/users', :notice => "Can only edit yourself."
     else
-      render 'edit'
+      @user.gmat = params[:gmat]
+      @user.gpa = params[:gpa]
+      @user.industry = params[:industry]
+      @user.collegerank = params[:collegerank]
+      @user.timeinservice = params[:timeinservice]
+      @user.major = params[ :major ]
+      @user.prohelp = params[ :prohelp ]
+
+      if @user.save
+        redirect_to "/users", :notice => "My stats were updated successfully."
+      else
+        render 'edit'
+      end
     end
+
   end
 
   def estimateProbabilities
@@ -46,6 +55,6 @@ class UsersController < ApplicationController
     else
       render 'estimateProbabilities'
     end
-    end
-
   end
+
+end
