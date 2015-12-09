4a3d7547ebfad21fdf4e325935db92aaac48e36a
Eric Klein <eric.j.klein@chicagobooth.edu>
2015-12-08 19:18:11 -0600

    unsuccessful activeadmin install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   # Routes for the Application resource:
   # CREATE
   get "/applications/new", :controller => "applications", :action => "new"
